69aebc1e7683ffbbcb9b3325d839ee16e4906381
Lilia <Liliapogosova@gmail.com>
2025-08-05 00:42:50 +0300

    ref4

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,7 @@
 import css from "./App.module.css";
 import CafeInfo from "../CafeInfo/CafeInfo";
 import VoteOptions from "../VoteOptions/VoteOptions"
-import type { Votes } from "../../types/votes";
-import type { VoteType } from "../../types/votes";
+import type { Votes, VoteType } from "../../types/votes";
 import { useState } from "react";
 import VoteStats from "../VoteStats/VoteStats"
 import Notification from "../Notification/Notification";
